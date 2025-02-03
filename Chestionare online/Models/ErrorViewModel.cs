4d59b3f755ceb4a4c39790f9e7fe4281c4fa6bf0
Andrei Balanoiu <andreibalanoiu67@gmail.com>
2025-02-03 22:05:41 +0200

    Add project files.

---
+++ Chestionare online/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Chestionare_online.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
